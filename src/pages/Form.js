c7896cd56eb07ef91477b46a2ed1eea939c25a0b
DenisaZem <dharibo@seznam.cz>
2023-12-01 22:44:56 +0100

    Form page filled_draft

--- src/pages/Form.js
+++ src/pages/Form.js
@@ -3,11 +3,38 @@ import { useState } from "react"
 import { projectFirestore } from "../firebase/config"
 
 const Form = () => {
-  return (
-    <div>
-      
-    </div>
-  )
+
+const[wordCze, setWordCze] = useState("")
+const[wordDe, setWordDe] = useState("")
+const[sentence, setSentence]=useState("")
+
+
+const submitForm = (e)=>{
+    e.preventDefault()
+}
+
+
+  return <form onSubmit={submitForm}>
+            <input 
+                type="text"
+                placeholder="České slovíčko" 
+             />
+             <br />
+            <input 
+                type="text"
+                placeholder="Němeské slovíčko" 
+             />
+             <br />
+            <input 
+                type="text"
+                placeholder="Vaše paměťová věta" 
+             />
+             <br />
+            <input 
+                type="submit" 
+                value="Přidat nové slovíčko do databáze" 
+            />   
+         </form>
 }
 
 export default Form
