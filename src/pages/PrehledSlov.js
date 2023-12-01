d378d9f8e8d4aded7a67fc22450e1dab692033b5
DenisaZem <dharibo@seznam.cz>
2023-12-01 17:15:45 +0100

    page:Prehled slov_connection with firebase

--- src/pages/PrehledSlov.js
+++ src/pages/PrehledSlov.js
@@ -1,9 +1,35 @@
 import "./PrehledSlov.css"
+import { projectFirestore } from "../firebase/config"
+import { useState, useEffect } from "react"
+import { Link } from "react-router-dom"
 
 const PrehledSlov = () => {
+
+  const [data, setData] = useState([]);
+  const [error, setError] = useState("");
+
+   useEffect (()=>{
+    projectFirestore.collection("deutsch").get().then((snapshot)=>{
+      console.log(snapshot)
+
+      if(snapshot.empty){
+        setError("Žádná slovíčka k zobrazení")
+      } else{
+        let result = []
+        snapshot.docs.forEach((oneWord)=>{
+          // console.log(oneWord.data())
+          result.push({id:oneWord.id, ...oneWord.data()})
+          console.log(result)
+
+        })
+      }
+
+    })
+  })
+
   return (
     <div>
-      Prehled Slov
+    {error && <p>{error}</p>}
     </div>
   )
 }
