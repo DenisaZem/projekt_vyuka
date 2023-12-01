540078ec69d0e639d32f1c71bd9fa22d2b29fce7
DenisaZem <dharibo@seznam.cz>
2023-12-01 22:31:14 +0100

    page:DetailSlova_connection with firebase

--- src/pages/DetailSlova.js
+++ src/pages/DetailSlova.js
@@ -1,16 +1,36 @@
 import "./DetailSlova.css"
 import { useParams } from "react-router-dom"
+import { useState, useEffect } from "react"
+import { projectFirestore } from "../firebase/config"
+
 
 const DetailSlova = () => {
+  const [data, setData] = useState([]);
+  const [error, setError] = useState("");
 
   const { wordId } = useParams()
 
+  useEffect(()=>{
+    projectFirestore.collection("deutsch").doc(wordId).get().then((document)=>{
+      console.log(document)
 
-  return (
-    <div>
-      {wordId}
-    </div>
-  )
+      if(document.exists){
+        setData(document.data())
+      }else{
+        setError ("Pamětová věta nelze zobrazit")
+      }
+    }).catch((err)=>{
+      setError(err.message)
+    })
+
+  },[])
+
+
+  return <section>
+    <h1>{data.wordCze}</h1>
+    <h3>{data.wordDe}</h3>
+    <p>{data.sentence}</p>
+  </section>
 }
 
 export default DetailSlova
