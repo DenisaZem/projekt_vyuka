fd120d00ebd665be324a41012dcc3a450f94c70c
DenisaZem <dharibo@seznam.cz>
2023-12-17 15:34:29 +0100

    fix button menu

--- src/components/Navigace.js
+++ src/components/Navigace.js
@@ -1,21 +1,30 @@
 import { NavLink } from "react-router-dom"
 import { GiHamburgerMenu } from "react-icons/gi"
+import{ useState } from "react"
 import "./Navigace.css"
 
 const Navigace = () => {
+  const[showMenu, setShowMenu] =useState(false)
+
+
   return <header>
                 {/* <img src="./pikto.png" className="photoB"/> */}
                 {/* <img src="https://www.obrazkyvevyuce.cz/user/16840/upload/ftp_client/piktogramy.png" className="mainIcon"/> */}
-                <nav>
+                <nav className={`${showMenu ? "nav show" : "nav hide"}` }>
                 {/* <img src="./knihy.jpg" alt="" /> */}
                 <img src="https://www.kampomaturite.cz/data/USR_052_DEFAULT/jazyky_multidimenzionalni_iq_11_2018_9597030_xxl.jpg"className="iconBook" alt="kniha" />
+                <div>
                 <NavLink to="/">Hlavní stránka</NavLink>
                 <NavLink to="review">Přehled slovíček</NavLink>
                 <NavLink to="form">Přidat nové slovíčko</NavLink>
-                <button>
-                    <GiHamburgerMenu/>
-                </button>
+                </div>
                 </nav>
+                <div className="justBut">
+                <button onClick={()=>setShowMenu(!showMenu)}>
+                    <GiHamburgerMenu className="buttonHam"/>
+                </button>
+                </div>
+               
         </header>
 
 
