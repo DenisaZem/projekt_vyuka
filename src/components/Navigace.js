57bf842ad9ce78a362e52f2fdc2752a91f733b87
DenisaZem <dharibo@seznam.cz>
2024-01-09 20:32:51 +0100

    Fix component navigation

--- src/components/Navigace.js
+++ src/components/Navigace.js
@@ -40,11 +40,13 @@ useEffect(() => {
   return (
     <header>
       <nav className={`menu-overlay ${showMenu ? "show" : "hide"}`}>
-        <img
-          src="https://img.freepik.com/free-vector/online-education-cartoon-concept-with-people-learning-online-vector-illustration_1284-84460.jpg?w=1380&t=st=1703592219~exp=1703592819~hmac=fe62f9ff88493f74888e4fb2a22f0425f10da0d31cd47180961cf14ea001a4d9"
-          className="iconBook"
-          alt="onlineEdu"
-        />
+      <NavLink to="/">
+          <img
+            src="https://img.freepik.com/free-vector/online-education-cartoon-concept-with-people-learning-online-vector-illustration_1284-84460.jpg?w=1380&t=st=1703592219~exp=1703592819~hmac=fe62f9ff88493f74888e4fb2a22f0425f10da0d31cd47180961cf14ea001a4d9"
+            className="iconBook"
+            alt="onlineEdu"
+          />
+        </NavLink>
         <div className={`work-menu ${showMenu ? "open" : ""}`}>
           <div className="menu-overlay"></div>
           <div className="nav-menu">
@@ -67,18 +69,20 @@ useEffect(() => {
         </div>
       </nav>
   <div className="nav-menu-button">
-  <img
-    src="https://img.freepik.com/free-vector/online-education-cartoon-concept-with-people-learning-online-vector-illustration_1284-84460.jpg?w=1380&t=st=1703592219~exp=1703592819~hmac=fe62f9ff88493f74888e4fb2a22f0425f10da0d31cd47180961cf14ea001a4d9"
-    className="iconBook2"
-    alt="onlineEdu2"
-  />
-    <button className="menuButton" onClick={handleMenuToggle}>
-      {showMenu ? (
-        <GiCancel className="buttonHam" />
-      ) : (
-        <GiHamburgerMenu className="buttonHam" />
-      )}
-    </button>
+  <NavLink to="/">
+          <img
+            src="https://img.freepik.com/free-vector/online-education-cartoon-concept-with-people-learning-online-vector-illustration_1284-84460.jpg?w=1380&t=st=1703592219~exp=1703592819~hmac=fe62f9ff88493f74888e4fb2a22f0425f10da0d31cd47180961cf14ea001a4d9"
+            className="iconBook2"
+            alt="onlineEdu2"
+          />
+        </NavLink>
+        <button className="menuButton" onClick={handleMenuToggle}>
+          {showMenu ? (
+            <GiCancel className="buttonHam" />
+          ) : (
+            <GiHamburgerMenu className="buttonHam" />
+          )}
+        </button>
   </div>
    
     </header>
